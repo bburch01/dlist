ed2d165a567dec522482b55697ec00a5cd600520
Barry Burch <barry@sbcglobal.net>
2019-09-06 13:37:42 -0500

    Continue dlist development

--- cmd/listDeps.go
+++ cmd/listDeps.go
@@ -18,7 +18,6 @@ import (
 	"log"
 
 	"github.com/bburch01/dlist/internal/app/dlist"
-
 	"github.com/spf13/cobra"
 )
 
@@ -45,14 +44,43 @@ var listDepsCmd = &cobra.Command{
 	Long:  `Lists all runtime required dependencies for a give CPAN perl distribution name.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		log.Println("stub: list dependencies here....")
+		//log.Println("stub: list dependencies here....")
 
-		log.Printf("MDM.BHooksEndOfScope:  %v", dlist.MDM.BHooksEndOfScope)
-		log.Printf("CoreModulesMap[CPAN::Complete]: %v", dlist.CoreModulesMap["CPAN::Complete"])
+		//log.Printf("MDM.BHooksEndOfScope:  %v", dlist.MDM.BHooksEndOfScope)
+		//log.Printf("CoreModulesMap[CPAN::Complete]: %v", dlist.CoreModulesMap["CPAN::Complete"])
 
 		//fmt.Println(result["users"])
 
-		log.Printf("Result[author]: %v", dlist.Result["author"])
+		//log.Printf("Result[author]: %v", dlist.Result["author"])
+
+		/*
+			log.Printf("metadata file count: %v", len(dlist.DistroMetaDataFilepathMap))
+
+			for k, v := range dlist.DistroMetaDataFilepathMap {
+				log.Printf("k: %v v: %v", k, v)
+			}
+		*/
+
+		for k, v := range dlist.DistroMetaDataMap {
+
+			switch k {
+			case "DateTime":
+				//var r = make(map[string]interface{})
+				r := v["prereqs"].(map[string]interface{})
+				s := r["runtime"].(map[string]interface{})
+				t := s["requires"].(map[string]interface{})
+				//log.Printf("DateTime author: %v", v["prereqs"])
+				//log.Printf("DateTime prereqs configure requires: %v", s["requires"])
+				//log.Printf("DateTime prereqs configure requires: %v", t)
+
+				for k, _ := range t {
+					log.Printf("required module: %v", k)
+				}
+
+			default:
+			}
+
+		}
 
 		return nil
 	},
--- internal/app/dlist/distro.go
+++ internal/app/dlist/distro.go
@@ -5,21 +5,75 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
-var Result map[string]interface{}
+var DistroMetaDataFilepathMap = make(map[string]string)
+
+var DistroMetaDataMap = make(map[string]map[string]interface{})
 
 func init() {
 
-	jsonFile, err := os.Open("data/DateTime/META.json")
+	//distroMetaDataFilepathMap := make(map[string]string)
 
+	err := filepath.Walk("data", func(path string, info os.FileInfo, err error) error {
+
+		if info.IsDir() {
+
+			if info.Name() != "data" {
+
+				DistroMetaDataFilepathMap[info.Name()] = filepath.Join("data", info.Name(), "META.json")
+
+			}
+
+		}
+
+		//files = append(files, path)
+		return nil
+	})
+
+	// TODO: add a proper error handler
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	defer jsonFile.Close()
+	//DistroMetaDataMap = make(map[string]interface{})
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	for k, v := range DistroMetaDataFilepathMap {
 
-	json.Unmarshal([]byte(byteValue), &Result)
+		var result map[string]interface{}
+
+		//fmt.Printf("\nfilepath to open: %v", v)
+
+		jsonFile, err := os.Open(v)
+
+		// TODO: add proper error handler
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer jsonFile.Close()
+
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		json.Unmarshal([]byte(byteValue), &result)
+
+		DistroMetaDataMap[k] = result
+
+	}
+
+	/*
+		jsonFile, err := os.Open("data/DateTime/META.json")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer jsonFile.Close()
+
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		json.Unmarshal([]byte(byteValue), &Result)
+
+	*/
 }
